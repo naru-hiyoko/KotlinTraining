bb53181f37c7a0147a2dc546474f0721a494975b
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2018-04-19 11:32:55 +0900

    exircise 1 まで

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -4,20 +4,18 @@ fun welcome() {
     println("welcome to ACCESS!")
 }
 
-fun hello()
-{
+fun hello() {
     println("Hello World!")
 
 }
 // TODO: declare hello function
 
-fun fizzbuzz(i: Int): String
-{
-    val a = if ( i % 15 == 0) {
+fun fizzbuzz(i: Int): String {
+    val a = if (i % 15 == 0) {
         "FizzBuzz"
-    } else if (i%5 == 0){
+    } else if (i % 5 == 0) {
         "Buzz"
-    } else if (i%3==0) {
+    } else if (i % 3 == 0) {
         "Fizz"
     } else {
         i.toString()
@@ -25,3 +23,32 @@ fun fizzbuzz(i: Int): String
 
     return a
 }
+
+fun isLeapYear(y: Int): Boolean {
+    if (y % 4 == 0) {
+        if (y % 100 == 0 && y % 400 != 0) {
+            return false
+        } else {
+            return true
+        }
+    } else {
+        return false
+    }
+}
+
+fun power(a: Int, n: Int): Long {
+    if (a < 0 || n < 0) {
+        println("inputs must be positive. return 0")
+        return 0
+    }
+    // TODO: 実装する
+
+    var arr = ArrayList<Int>();
+    for (i in 1..n) {
+        arr.add(a);
+    }
+
+    return arr.reduce { acc, a -> acc * a }.toLong();
+
+}
+
--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -22,10 +22,8 @@ class SampleTest {
     }
 
     @Test
-    fun testFizzBuzz()
-    {
-        for (i in 1..100)
-        {
+    fun testFizzBuzz() {
+        for (i in 1..100) {
             val ret = fizzbuzz(i);
             //println(ret)
         }
@@ -34,7 +32,31 @@ class SampleTest {
         assertEquals("Fizz", fizzbuzz(6));
         assertEquals("Buzz", fizzbuzz(10));
 
+    }
 
+    @Test
+    fun testIsLeapYear() {
+        // 閏年で無い
+        assertEquals(false, isLeapYear(1700))
+        assertEquals(false, isLeapYear(1800))
+        assertEquals(false, isLeapYear(1900))
 
+        assertEquals(false, isLeapYear(2100))
+        assertEquals(false, isLeapYear(2200))
+        assertEquals(false, isLeapYear(2300))
+
+        assertEquals(false, isLeapYear(2500))
+        assertEquals(false, isLeapYear(2600))
+
+        //閏年
+        assertEquals(true, isLeapYear(1600))
+        assertEquals(true, isLeapYear(2000))
+        assertEquals(true, isLeapYear(2400))
+
+    }
+
+    @Test
+    fun testPowerFunc() {
+        assertEquals(9.toLong(), power(3, 2))
     }
 }
