74c0f58d9027b96e32a7c004aa378fa144e9b2c3
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2018-04-20 17:30:01 +0900

    クラスファイルに変更を加えました

--- app/src/main/java/io/access/kotlintraining/Dice.kt
+++ app/src/main/java/io/access/kotlintraining/Dice.kt
@@ -1,4 +1,39 @@
 package io.access.kotlintraining
 
+import java.util.*
+
 class Dice {
+    var n: Int
+    private var trialLimit = 0
+
+    internal var counter: Int = 0
+
+    var propertyWithCounter: Int = 0
+        set(value) {
+            this.counter += 1
+        }
+
+
+    constructor(n: Int) {
+        this.n = n
+    }
+
+    constructor(n: Int, trial: Int) {
+        this.n = n
+        this.trialLimit = trial
+    }
+
+    init {
+        //
+    }
+
+    fun roll(): Int {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (trialLimit >= 100) {
+            throw Exception("I was broken")
+        }
+        trialLimit += 1
+        return Random().nextInt(this.n) + 1;
+    }
+
 }
--- app/src/main/java/io/access/kotlintraining/Nabeatsu.kt
+++ app/src/main/java/io/access/kotlintraining/Nabeatsu.kt
@@ -1,4 +1,27 @@
 package io.access.kotlintraining
 
-class Nabeatsu {
+class NabeAtsu {
+    var counter: Int = 0
+
+    constructor() {
+        //
+    }
+
+    constructor(startWith: Int) {
+        this.counter = startWith
+    }
+
+    fun next(): String {
+        this.counter += 1
+
+        if (isAho(this.counter)) {
+            return "Aho"
+        } else {
+            return this.counter.toString()
+        }
+    }
+
+    fun isAho(i: Int): Boolean {
+        return this.counter % 3 == 0 || this.counter.toString().contains("3")
+    }
 }
