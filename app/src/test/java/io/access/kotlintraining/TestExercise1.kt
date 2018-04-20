8f2b0f7b7641b44146ce4aa5f2387bdebcfc615b
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2018-04-20 17:29:31 +0900

    テスト用のスクリプトを追加

--- app/src/test/java/io/access/kotlintraining/TestExercise1.kt
+++ app/src/test/java/io/access/kotlintraining/TestExercise1.kt
@@ -1,4 +1,56 @@
 package io.access.kotlintraining
 
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * Exercise1 の回答です。
+ * */
+
 class TestExercise1 {
+
+    @Test
+    fun testHello() {
+        hello()
+    }
+
+    @Test
+    fun testFizzBuzz() {
+        for (i in 1..100) {
+            val ret = fizzbuzz(i);
+            //println(ret)
+        }
+
+        assertEquals("FizzBuzz", fizzbuzz(15));
+        assertEquals("Fizz", fizzbuzz(6));
+        assertEquals("Buzz", fizzbuzz(10));
+
+    }
+
+    @Test
+    fun testIsLeapYear() {
+        // 閏年で無い
+        assertEquals(false, isLeapYear(1700))
+        assertEquals(false, isLeapYear(1800))
+        assertEquals(false, isLeapYear(1900))
+
+        assertEquals(false, isLeapYear(2100))
+        assertEquals(false, isLeapYear(2200))
+        assertEquals(false, isLeapYear(2300))
+
+        assertEquals(false, isLeapYear(2500))
+        assertEquals(false, isLeapYear(2600))
+
+        //閏年
+        assertEquals(true, isLeapYear(1600))
+        assertEquals(true, isLeapYear(2000))
+        assertEquals(true, isLeapYear(2400))
+
+    }
+
+    @Test
+    fun testPowerFunc() {
+        assertEquals(9.toLong(), power(3, 2))
+    }
+
 }
--- app/src/test/java/io/access/kotlintraining/TestExercise2.kt
+++ app/src/test/java/io/access/kotlintraining/TestExercise2.kt
@@ -1,4 +1,46 @@
 package io.access.kotlintraining
 
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/**
+ * Exercise 2 の回答です。
+ * */
+
 class TestExercise2 {
+
+    @Test
+    fun testIsOdd()
+    {
+        assertEquals(true, 3.isOdd())
+        assertEquals(true, 4.isEven())
+    }
+
+    @Test
+    fun testDice() {
+        // インスタンスの生成, 3面のサイコロで 98 回目から開始する。 100 回目で例外が発生する。
+        var d = Dice(3, 98)
+
+        try {
+            println(d.roll())
+            println(d.roll())
+            println(d.roll())
+        } catch (e: Exception) {
+            println(e)
+        }
+
+        d.propertyWithCounter = 100
+        d.propertyWithCounter = 200
+        assertEquals(d.counter, 2)
+    }
+
+    @Test
+    fun testNabeatsu() {
+        // ナベアツの問題、
+        val n = NabeAtsu(startWith = 21);
+        assertEquals(n.next(), "22")
+        assertEquals(n.next(), "Aho")
+        assertEquals(n.next(), "Aho")
+    }
+
 }
